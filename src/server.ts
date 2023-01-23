2eb517ea19077aa53afbdd3b00633a9ec4912f30
Reza Heidari <reza.heidari@amplicade.com>
2023-01-23 16:38:20 +0300

    migrate to typescript

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,4 @@
-const express = require("express");
+import express from "express";
 
 const app = express();
 
@@ -7,4 +7,4 @@ app.get("/", (req, res) => {
   res.status(200).json({ message: "Hey there..." });
 });
 
-module.exports = app;
+export default app;
