26ec8c9a7b9933f22a15bd6fec5f16cde49f1e32
Reza Heidari <reza.heidari@amplicade.com>
2023-01-23 23:40:00 +0300

    add jwt for auth

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,8 @@
 import server from './server';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 server.listen(3000, () => {
-    console.log('Server is running on port 3000'); 
+    console.log('Server is running on port 3000');
 });
--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,21 @@
 import express from "express";
+import morgan from 'morgan';
+import cors from 'cors';
+import router from './router';
+import { protect } from "./modules/auth";
 
 const app = express();
+
+app.use(cors());
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
   console.log("Hello from Express!");
   res.status(200).json({ message: "Hey there..." });
 });
+
+app.use('/api', protect, router);
 
 export default app;
